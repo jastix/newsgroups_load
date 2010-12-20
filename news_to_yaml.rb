f294ff23bd46089e3ae709234f70d7cc686db977
Nikolay Petrachkov <nik@jastix.me>
2010-12-20 17:38:14 +0100

    progress

--- news_to_yaml.rb
+++ news_to_yaml.rb
@@ -13,15 +13,24 @@ email, name = Array.new
 Dir.chdir('test')
 categories = Dir.glob("**/").each {|x| x.chop!} #list of categories
 Dir.glob("**/").each do |entry|
-    Dir.open(entry) do |dir|
-      dir.each do |file| #open every file in directory
-        File.open(file.path, "r") do |line|
+    Dir.chdir(entry)
+    puts Dir.pwd
+    Dir.entries(Dir.pwd).each {|file|
+         #open every file in directory
+
+        f = File.open(file, "r") unless !File.directory?(file)
+        f.each_line {|line|
+        puts line
           if line.starts_with?('from')
             line.split
 
-        puts file
+
+        end
+        }}
+
       end
   end
+  Dir.chdir('..')
 end
 Dir.chdir('..')
 
